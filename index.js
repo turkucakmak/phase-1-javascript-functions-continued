dece4557bf258ee385502c3a085a4fbc6ca63927
turku <turkucakmak@hotmail.com>
2023-05-20 17:13:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(string="roller-skate"){
+    return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(string="*"){
+    return inner;
+    function inner(single="special"){
+        return `You are ${string}${single}${string}!`;
+
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
